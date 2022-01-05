c5182d83e030d05ba9b27073cbc61f4f94ceb0bc
James Mitchell <JamesMitchell@Rocketmortgage.com>
2022-01-05 16:36:35 -0500

    first commit

---
+++ GCCarDealershipAPI/Controllers/LotController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GCCarDealershipAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LotController : ControllerBase
+    {
+        Inventory cl = new Inventory();
+        
+        [HttpGet]
+        public List<Car> GetAllCars()
+        {
+            return cl.AllCars;
+        }
+        [HttpGet("GetCarByMake/{make}")]
+        public List<Car> GetCarsByMake(string make)
+        {
+            List<Car> cars = new List<Car>();
+            foreach(Car c in cl.AllCars)
+            {
+                if(c.Make.Contains(make) || c.Make.ToLower() == make.ToLower())
+                {
+                    cars.Add(c);
+                }
+            }
+            return cars;
+        }
+        [HttpGet("GetCarsByColor/{color}")]
+        public List<Car> GetCarsByColor(string color)
+        {
+            List<Car> cars = new List<Car>();
+            foreach(Car c in cl.AllCars)
+            {
+                if(c.Color.Contains(color) || c.Color.ToLower() == color.ToLower())
+                {
+                    cars.Add(c);
+                }
+            }
+            return cars;
+        }
+        [HttpGet("GetCarByIndex/{index}")]
+        public string GetCarByIndex(int index)
+        {
+            Car errorCar = new Car("Error", "Index", index, "Is Invalid, Try Again");
+            try
+            {
+                return cl.AllCars[index].ToString();
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return $"{index} is not a valid Car ID. Please select a number between 0 - {cl.AllCars.Count-1}";
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return $"{index} is not a valid Car ID. Please select a number between 0 - {cl.AllCars.Count - 1}";
+            }
+        }
+        [HttpGet("SearchByAnything/{keyword}")]
+        public List<Car> SearchByAnything(string keyword)
+        {
+            List<Car> toReturn = new List<Car>();
+            keyword = keyword.Trim().ToLower();
+            foreach(Car c in cl.AllCars)
+            {
+                string year = c.Year.ToString();
+                if ((c.Color.ToLower() == keyword || c.Color.ToLower().Contains(keyword)) || 
+                    (c.Make.ToLower() == keyword || c.Make.ToLower().Contains(keyword)) || 
+                    (c.Model.ToLower() == keyword || c.Model.ToLower().Contains(keyword) || 
+                    (year == keyword)))
+                    {
+                    toReturn.Add(c);
+                }            
+            }
+            return toReturn;
+        }
+    }
+}
---
+++ GCCarDealershipAPI/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GCCarDealershipAPI
+{
+    public class Inventory
+    {
+        public List<Car> AllCars { get; set; } = new List<Car>();
+        public Inventory()
+        {
+            AllCars.Add(new Car("Toyota", "Hilux", 1994, "White"));
+            AllCars.Add(new Car("Toyota", "Corola", 1994, "Rust and Invisible"));
+            AllCars.Add(new Car("Honda", "Civic", 1994, "Yellow"));
+            AllCars.Add(new Car("Geo", "Metro", 1998, "White"));
+            AllCars.Add(new Car("Toyota", "Hilux", 1999, "White"));
+            AllCars.Add(new Car("Toyota", "T100", 1994, "White"));
+            AllCars.Add(new Car("Mercury", "Hilux", 1994, "White"));
+        }
+    }
+}
